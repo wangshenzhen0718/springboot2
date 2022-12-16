67b1011e9a5aac9365e54e65bd008b5615c99b22
wangshenzhen0718 <153474100@qq.com>
2022-12-16 16:10:55 +0800

    @Import

--- boot-01-helloworld/src/main/java/com/wang/boot/config/MyConfig.java
+++ boot-01-helloworld/src/main/java/com/wang/boot/config/MyConfig.java
@@ -1,9 +1,12 @@
 package com.wang.boot.config;
 
+import ch.qos.logback.core.db.DBHelper;
 import com.wang.boot.bean.Pet;
 import com.wang.boot.bean.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
 /**
  * 1、配置类里面使用@Bean标注在方法上给容器注册组件，默认也是单实例的
  * 2、配置类本身也是组件
@@ -11,7 +14,10 @@ import org.springframework.context.annotation.Configuration;
  *      Full(proxyBeanMethods = true)、【保证每个@Bean方法被调用多少次返回的组件都是单实例的】
  *      Lite(proxyBeanMethods = false)【每个@Bean方法被调用多少次返回的组件都是新创建的】
  *      组件依赖必须使用Full模式默认。其他默认是否Lite模式
+ * 4、@Import({User.class, DBHelper.class})
+ *       给容器中自动创建出这两个类型的组件、默认组件的名字就是全类名
  **/
+@Import({User.class, DBHelper.class})
 @Configuration(proxyBeanMethods = false) //告诉SpringBoot这是一个配置类 == 配置文件
 public class MyConfig {
 
