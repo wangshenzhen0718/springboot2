a1b012534a853398096a4305e2dc103fc4c5d32a
wangshenzhen0718 <153474100@qq.com>
2022-12-17 14:24:25 +0800

    format

--- boot-01-helloworld/src/main/java/com/wang/boot/controller/HelloController.java
+++ boot-01-helloworld/src/main/java/com/wang/boot/controller/HelloController.java
@@ -1,16 +1,18 @@
 package com.wang.boot.controller;
 
 import com.wang.boot.bean.Car;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 public class HelloController {
     @Autowired
     private  Car car;
     @RequestMapping("/hello")
     public String sayHello(){
+        log.info("请求进来了");
 
         return "hello springboot2";
 
